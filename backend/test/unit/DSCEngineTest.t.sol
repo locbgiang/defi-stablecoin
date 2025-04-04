e77f947e214b24efb5b0b313492d370bebfbbc10
locgiang <locbgiang@gmail.com>
2025-04-03 23:29:58 -0700

    dscengine test start

--- backend/test/unit/DSCEngineTest.t.sol
+++ backend/test/unit/DSCEngineTest.t.sol
@@ -1,11 +1,56 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import { DeployDsc } from "../../script/DeployDSC.s.sol";
-import { DSCEngine } from "../../src/DSCEngine.sol";
-import { DecentralizedStableCoin } from "../../src/DecentralizedStableCoin.sol";
-import { HelperConfig } from "../../script/HelperConfig.s.sol";
-// import { ERC20Mock } from "@openzeppelin/contracts/mocks/ERC20Mock.sol"; Updated mock location
-import { ERC20Mock } from "../mocks/ERC20Mock.sol";
-import { MockV3Aggregator } from "../mocks/MockV3Aggregator.sol";
-import { MockMoreDebtDSC } from "../mocks/MockMoreDebtDSC.sol";
+contract DSCEngineTest {
+    function setUp() external {}
+
+    ///////////////////////
+    // Constructor Tests //
+    ///////////////////////
+
+    function testRevertsIfTokenLengthDoesntMatchPriceFeed() public {}
+
+    /////////////////
+    // Price Tests //
+    /////////////////
+
+    function testGetTokenAmountFromUsd() public {}
+
+    function testGetUsdValue() public {}
+
+    /////////////////////////////
+    // depositCollateral Tests //
+    /////////////////////////////
+
+    
+
+    ///////////////////////////////////////
+    // depositCollateralAndMintDsc Tests //
+    ///////////////////////////////////////
+
+    ///////////////////
+    // mintDsc Tests //
+    ///////////////////
+
+    ///////////////////
+    // burnDsc Tests //
+    ///////////////////
+
+    ////////////////////////////
+    // redeemCollateral Tests //
+    ////////////////////////////
+
+    ////////////////////////
+    // healthFactor Tests //
+    ////////////////////////
+
+    ///////////////////////
+    // Liquidation Tests //
+    ///////////////////////
+
+    ////////////////////////////////
+    // View & Pure Function Tests //
+    ////////////////////////////////
+
+
+}
