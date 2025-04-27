e353dab59bcebf147c0606408546d81b16c7d683
locbgiang <locbgiang@gmail.com>
2025-04-27 00:46:04 -0700

    reset basic css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,11 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import { Web3Provider } from './contexts/Web3Context';
-import { CollateralManagement } from './components/CollateralManagement';
-import { UserDashboard } from './components/UserDashboard';
-import { Box, Container, Button } from '@chakra-ui/react';
-import { useWeb3 } from './contexts/Web3Context';
+import { Header } from "./components/Header";
 
 function App() {
-    const { account, connectWallet } = useWeb3();
-
     return (
-        <ChakraProvider>
-            <Web3Provider>
-                <Container maxW="container.xl" py={8}>
-                    {!account ? (
-                        <Button onClick={connectWallet}>
-                            Connect Wallet
-                        </Button>
-                    ) : (
-                        <Box>
-                            <UserDashboard />
-                            <CollateralManagement />
-                        </Box>
-                    )}
-                </Container>
-            </Web3Provider>
-        </ChakraProvider>
+        <div>
+            <Header />
+            Hello world
+        </div>
     );
 }
 
