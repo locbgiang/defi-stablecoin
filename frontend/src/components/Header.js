b57cfb72176ae4de1fd63b5e3b14c0b34e079e63
locbgiang <locbgiang@gmail.com>
2025-04-27 23:53:44 -0700

    button

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 export const Header = () => {
+    const [account, setAccount] = useState('');
+
+    const connectWallet = async () => {
+        if (window.ethereum) {
+            try {
+                // Request account access 
+                const accounts = await window.ethereum.request({
+                    method: 'eth_requestAccounts'
+                });
+                setAccount(accounts[0]);
+            } catch (error) {
+                console.error("User denied account access");
+            }
+        } else {
+            alert('Please install MetaMask!');
+        }
+    }; 
+
     return (
         <div className="header">
-            header
+            <div className="logo">Decentralized Stablecoin</div>
+            <button 
+                className="connect-button"
+                onClick={connectWallet}
+            >
+                {account ? `${account.substring(0, 6)}...${account.substring(account.length - 4)}` : 'Connect Wallet'}
+            </button>
         </div>
     )
 }
-
