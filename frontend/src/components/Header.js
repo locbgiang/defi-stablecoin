8d85c4bf4faadd4c9feea06450d35b7ead1ab62f
locbgiang <locbgiang@gmail.com>
2025-05-01 00:01:21 -0700

    fixed button

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,6 +3,7 @@ import './Header.css';
 
 export const Header = () => {
     const [account, setAccount] = useState('');
+    const [isHovered, setIsHovered] = useState(false); // State to track hover
 
     const connectWallet = async () => {
         if (window.ethereum) {
@@ -26,8 +27,16 @@ export const Header = () => {
             <button 
                 className="connect-button"
                 onClick={connectWallet}
+                onMouseEnter={() => setIsHovered(true)} // Set hover state to true
+                onMouseLeave={() => setIsHovered(false)} // Set hover state to false
             >
-                {account ? `${account.substring(0, 6)}...${account.substring(account.length - 4)}` : 'Connect Wallet'}
+                {
+                account 
+                    ? isHovered // if account is connected check hover state
+                        ? account // show full account on hover
+                        : `${account.substring(0, 6)}...${account.substring(account.length - 4)}` 
+                    : 'Connect Wallet'
+                }
             </button>
         </div>
     )
